c016e24b35db22d6ea7934f13d0414c418673f20
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2022-02-22 18:53:58 -0800

    Event tests and Numberofevents tests

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+     state = {
+         numberOfEvents: '50',
+       }
+
+       handleInputChange = (event) => {
+        this.setState({
+            numberOfEvents: event.target.value,
+          });
+
+    }
+
+
+
+    render() {
+      
+     return(
+      
+      <div className="NumberOfEvents">
+          <input 
+          type="number" 
+          className="numberOfEvents"
+          value={this.state.numberOfEvents} 
+          onChange={this.handleInputChange}
+          />
+      </div>
+
+      )
+ }};
+
+
+ export default NumberOfEvents;
+
---
+++ src/_tests_/NumberOfEvents.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe("<NumberOfEvents /> component", () => {
+ let NumberOfEventsWrapper;
+
+ beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+ });
+
+ test("render the text input of number", () => {
+  expect(NumberOfEventsWrapper.find(".numberOfEvents")).toHaveLength(1);
+ })
+
+  test("change state when number input changes in the text box", () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: "32" });
+    NumberOfEventsWrapper.find(".numberOfEvents").simulate("change", {
+      target: { value: "13" },
+    });
+    expect(NumberOfEventsWrapper.state("numberOfEvents")).toEqual("13");
+  });
+});
+
