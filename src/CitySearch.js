c05cbb881ba34b368e5c1bd548087954e62b17b2
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2022-02-23 18:34:19 -0800

    minor changes

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -51,7 +51,7 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key="all">
+          <li onClick={() => this.handleItemClicked("all")}>
             <b>see all cities</b>
           </li>
         </ul>
--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -62,4 +62,13 @@ describe("<App /> integration", () => {
     console.error(error);
     AppWrapper.unmount();
   });
+
+  test('get list of all events when user selects "see all cities"', async () => {
+    const AppWrapper = mount(<App />);
+    const suggestionItems = AppWrapper.find(CitySearch).find(".suggestions li");
+    await suggestionItems.at(suggestionItems.length - 1).simulate("click");
+    const allEvents = await getEvents();
+    expect(AppWrapper.state("events")).toEqual(allEvents);
+    AppWrapper.unmount();
+  });
 });
