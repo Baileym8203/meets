189d2d06ff145dcd14f23eb1259b29df8cc1da98
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2022-03-12 18:19:23 -0800

    adding in a scatterchart and piechart for info on events

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import WelcomeScreen from "./WelcomeScreen";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import EventGenre from "./EventGenre";
 
 import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import { InfoAlert } from "./Alert";
@@ -64,12 +74,26 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
     const { events, locations, numberOfEvents } = this.state;
     return (
       <div className="App">
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <InfoAlert />
         <CitySearch
           locations={locations}
@@ -81,6 +105,28 @@ class App extends Component {
             this.updateNumberOfEvents(number);
           }}
         />
+        <div className="data-vis-wrapper">
+          <h4>Popularity of the events</h4>
+          <EventGenre events={events} />
+          <h4>Events in each city</h4>
+          <ResponsiveContainer height={400}>
+            <ScatterChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={events} numberOfEvents={numberOfEvents} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
